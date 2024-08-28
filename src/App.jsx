596edf75061c66e5c2090b73cab536ac1ed6b2f2
pedroso-miguell <miguellarapedroso37@gmail.com>
2024-08-27 23:03:28 -0300

    Revisando alguns conceitos!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import "./App.css";
 import React from "react";
 import inicial from "./assets/img/inicial.png";
 import quemsomos2 from "./assets/img/quemsomos2.png";
+import tela1 from "./assets/img/tela1.png";
+import tela2 from "./assets/img/tela2.png";
+import tela3 from "./assets/img/tela3.png";
+import tela4 from "./assets/img/tela4.png";
+import tela5 from "./assets/img/tela5.png";
+import tela6 from "./assets/img/tela6.png";
+import tela7 from "./assets/img/tela7.png";
+import tela8 from "./assets/img/tela8.png";
+import tela9 from "./assets/img/tela9.png";
+import tela10 from "./assets/img/tela10.png";
 import fundo3 from "./assets/img/fundo3.png";
 import objetivo from "./assets/img/objetivo.png";
 import proposta from "./assets/img/proposta.png";
@@ -9,9 +19,6 @@ import wireframes from "./assets/img/wireframes.png";
 import rodape from "./assets/img/rodape.png";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import tela1 from "./assets/img/tela1.png";
-
-
 
 function App() {
   return (
@@ -22,11 +29,10 @@ function App() {
           style={{ backgroundImage: `url(${inicial})` }}
         >
           <div className="apresentacao">
-            <p className="intro">Seu guarda roupa na palma da mão</p>
+            <p className="intro">Seu guarda-roupa na palma da mão</p>
             <button className="bnt1">
               <p className="embreve">Em Breve</p>
             </button>
-
             <button className="bnt2">
               <p className="colorido">Saiba mais</p>
             </button>
@@ -44,18 +50,12 @@ function App() {
         >
           <div className="apresentacao2">
             <p>
-              <p className="linha1">
-                A GoDress é uma equipe apaixonada por moda e
-              </p>{" "}
-              <p>tecnologia, comprometida em simplificar a maneira</p>{" "}
-              <p className="linha2">
-                como as pessoas se vestem para eventos especiais.
-              </p>{" "}
-              <p>Combinando inovação e praticidade, nosso objetivo é</p>
-              <p className="linha3">
-                oferecer uma solução completa para a gestão
-              </p>{" "}
-              <p className="linha4">inteligente do guarda-roupa.</p>
+             A GoDress é uma equipe apaixonada por moda e <br></br>
+             tecnologia, comprometida em simplificar a maneira
+             como as pessoas se vestem para eventos especiais.<br></br>
+             Combinando inovação e praticidade, nosso objetivo é
+            oferecer uma solução completa para a gestão<br></br>
+             inteligente do guarda-roupa.
             </p>
           </div>
         </div>
@@ -71,15 +71,15 @@ function App() {
         >
           <div className="titulo">O que será a GoDress</div>
           <div className="descricao">
-            <p>A GoDress será um aplicativo</p>{" "}
+            <p>A GoDress será um aplicativo</p>
             <p>inovador que permite aos usuários</p>
-            <p>organizar suas roupas de forma</p>{" "}
-            <p>eficiente, criar outfits incríveis e </p>{" "}
-            <p>receber sugestões personalizadas</p>{" "}
+            <p>organizar suas roupas de forma</p>
+            <p>eficiente, criar outfits incríveis e</p>
+            <p>receber sugestões personalizadas</p>
             <p>com base em suas preferências e</p>
-            <p>no tipo de evento. Com uma</p>{" "}
+            <p>no tipo de evento. Com uma</p>
             <p>interface intuitiva e recursos</p>
-            <p>avançados, a GoDress será o seu </p>{" "}
+            <p>avançados, a GoDress será o seu</p>
             <p>melhor aliado na hora de se vestir</p>
             <p>para qualquer ocasião.</p>
           </div>
@@ -96,12 +96,12 @@ function App() {
         >
           <div className="tituloobjetivo">Nosso Objetivo</div>
           <div className="textoobjetivo">
-            <p>Nosso principal objetivo é facilitar a vida</p>{" "}
-            <p>das pessoas, tornando o processo de</p>{" "}
-            <p>seleção de roupas mais fácil, eficiente e</p>{" "}
-            <p>divertido. Queremos ajudar nossos</p>{" "}
+            <p>Nosso principal objetivo é facilitar a vida</p>
+            <p>das pessoas, tornando o processo de</p>
+            <p>seleção de roupas mais fácil, eficiente e</p>
+            <p>divertido. Queremos ajudar nossos</p>
             <p>usuários a se sentirem confiantes e</p>
-            <p>bem-vestidos em qualquer ocasião, sem</p>{" "}
+            <p>bem-vestidos em qualquer ocasião, sem</p>
             <p>o estresse de escolher o outfit perfeito.</p>
           </div>
         </div>
@@ -117,14 +117,14 @@ function App() {
         >
           <div className="tituloproposta">Proposta</div>
           <div className="textoproposta">
-            <p>A GoDress propõe uma abordagem</p>{" "}
+            <p>A GoDress propõe uma abordagem</p>
             <p>revolucionária para a organização de</p>
-            <p>roupas, oferecendo um armário virtual</p>{" "}
+            <p>roupas, oferecendo um armário virtual</p>
             <p>inteligente que se adapta às</p>
-            <p>necessidades individuais de cada usuário.</p>{" "}
-            <p>Com recursos avançados como sugestões</p>{" "}
-            <p>personalizadas, integração meteorológica</p>{" "}
-            <p>e lavanderia digital, nossa proposta é</p>{" "}
+            <p>necessidades individuais de cada usuário.</p>
+            <p>Com recursos avançados como sugestões</p>
+            <p>personalizadas, integração meteorológica</p>
+            <p>e lavanderia digital, nossa proposta é</p>
             <p>revolucionar a maneira como as pessoas</p>
             <p>gerenciam seus guarda-roupas.</p>
           </div>
@@ -140,14 +140,61 @@ function App() {
           }}
         >
           <div className="titulowireframes">Wireframes</div>
-          <div className="App">
-     
-    </div>
-
+          <div className="carousel-container">
+            <Carousel
+              autoPlay
+              infiniteLoop
+              showThumbs={false}
+              showStatus={false}
+              interval={3000}
+            >
+              <div>
+                <img src={tela1} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela2} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela3} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela4} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela5} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela6} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela7} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela8} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela9} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+              <div>
+                <img src={tela10} alt="Wireframe 1" />
+                <p className="legend">Wireframe 1</p>
+              </div>
+             
+                
+            </Carousel>
+          </div>
           <button className="bntregistro">
-              <p>Pré Registro</p>
-            </button>
-
+            <p>Pré Registro</p>
+          </button>
         </div>
       </div>
 
@@ -161,10 +208,10 @@ function App() {
         >
           <div className="titulorodape">Experimente Agora!</div>
           <div className="textorodape">
-            <p>Faça parte da revolução na organização de roupas e torne</p> <p>seu
-            processo de seleção de looks mais fácil, eficiente e</p><p> divertido com a
-            GoDress. Cadastre-se agora e comece a </p><p className="linha5">transformar sua maneira de se
-            vestir!</p>
+            <p>Faça parte da revolução na organização de roupas e torne</p>
+            <p>seu processo de seleção de looks mais fácil, eficiente e</p>
+            <p>divertido com a GoDress. Cadastre-se agora e comece a</p>
+            <p className="linha5">transformar sua maneira de se vestir!</p>
           </div>
         </div>
       </div>
